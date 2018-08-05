49c97a33f0ce16cf122a54057e8347c2c30dc8d0
JarekZ92 <z.jarek92@gmail.com>
2018-08-05 17:13:15 +0200

    passing props down

--- src/Components/ToDo/ListOfTasks.js
+++ src/Components/ToDo/ListOfTasks.js
@@ -7,6 +7,7 @@ const ListOfTasks = (props) => (
        {props.tasksProp.map(task => (
        <SingleTask 
         nameProp = {task.name}
+        deleteTaskProp={() => props.deleteTaskProp(task.key)}
        />
        ))}
    </div>
