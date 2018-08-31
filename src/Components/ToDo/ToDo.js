7b53c692e573bcec8b7502d24fb03910d07a3d26
JarekZ92 <z.jarek92@gmail.com>
2018-08-31 12:07:35 +0200

    cleared testing tasks

--- src/Components/ToDo/ToDo.js
+++ src/Components/ToDo/ToDo.js
@@ -6,9 +6,9 @@ import PaperContainer from "../PaperContainer";
 class ToDo extends React.Component {
   state = {
     tasks: [
-      { name: "Posprzątaj mieszkanie", key: "123" },
-      { name: "Wyprowadź psa", key: "456" },
-      { name: "Umyj okna", key: "789" }
+      { name: "", key: "123" },
+      { name: "", key: "456" },
+      { name: "", key: "789" }
     ],
     newTaskName: ""
   };
